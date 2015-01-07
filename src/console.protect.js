7213ffad99edb457240c7eefd0178a0e541ec090
Jonathan Cousins <j.cousins@ry.com>
2015-01-07 17:11:51 +0000

    Minor code update

--- src/console.protect.js
+++ src/console.protect.js
@@ -3,8 +3,8 @@
   // Define an empty function
   var a = function(){};
 
-  // A list of all the console methods (as of Chrome 39.0.2171.95)
-  var methods = {
+  // A list of all the console methods (as of Chrome 39.0.2171.95 & 34.0.5)
+  var methods = [
     log,
     assert,
     debug,
@@ -28,17 +28,16 @@
     timeStamp,
     clear,
     memory,
-    table,
     timeline,
-    timelineEnd,
-  };
+    timelineEnd
+  ];
 
-  if (typeof window.console == "undefined") {
+  if (typeof window.console === "undefined") {
 
     // Loop through all the methods and define non-existing ones
     // using the empty function
     for(var i = 0; i < methods.length; i++) {
-      if(typeof window.console[methods[i]] == "undefined") {
+      if(typeof window.console[methods[i]] === "undefined") {
         window.console[methods[i]] = a;
       }
     }
